7361b149012ce5a2992b8701b9300a614c60c266
Albert Mazur <albert153@interia.pl>
2021-06-01 00:07:04 +0200

    Add animates in script

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
-$('article').hide().delay(200).fadeIn(2500);
-$('section[id="education"]').hide().delay(500).fadeIn(2500);
-$('section[id="skills"]').hide().delay(800).fadeIn(2500);
+$(function(){
+    //$('article').hide().delay(200).slideDown(2500);
+    //$('section').hide().delay(500).slideDown(2500);
+
+    $article = $('article');
+    $section = $("section");
+    $header = $('header');
+
+    $header.css('left', 2000);
+    $article.css("left", -2000);
+    $section.css("left", -2000);
+
+    $article.animate({
+        left: '0'
+    }, 1500, function(){} );
+
+
+    $header.animate({
+        left: '0'
+    }, 1500, function(){} );
+
+
+    $section.each(function(index){
+        $(this).delay(1000*(index+1)).animate({
+            left: '0'
+        }, 1500, function(){} );
+    });
+});
